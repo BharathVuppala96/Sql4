3860c521e476ebecad8aef2ea62c620db33b1d90
Bharath Vuppala <bharathv8696@gmail.com>
2024-12-20 19:52:21 -0800

    Changes made

--- Friend request II.sql
+++ Friend request II.sql
@@ -2,4 +2,4 @@ with cte as (select requester_id as r1 from requestaccepted
 union all
 select accepter_id as r2 from requestaccepted)
 
-select  r1 as 'id',count(r1) as num  from CTE WHERE = MAX(NUM)
+select  r1 as 'id',count(r1) as num  from CTE group by 1 order by num desc limit 1
